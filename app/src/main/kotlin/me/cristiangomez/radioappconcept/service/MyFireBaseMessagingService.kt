c73c259a61b3372d63cf7968c1bdf22f0457677c
Cristian Javier Gomez Mendez <cristiangomezmendez@gmail.com>
2018-08-01 22:22:37 -0400

    feature: as a radio broadcaster i want to send messages to my listeners
    implementing using firebase
    
    closes #4

---
+++ app/src/main/kotlin/me/cristiangomez/radioappconcept/service/MyFireBaseMessagingService.kt
@@ -0,0 +1,6 @@
+package me.cristiangomez.radioappconcept.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFireBaseMessagingService: FirebaseMessagingService() {
+}
